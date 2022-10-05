688e774bc2d5537d92f52096c607ffae7a331de1
Phạm Văn Nguyên <73846954+npv2k1@users.noreply.github.com>
2022-10-05 12:45:29 +0700

    update

--- src/main/java/org/example/SocketClient.java
+++ src/main/java/org/example/SocketClient.java
@@ -1,14 +1,25 @@
-package org.example;
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
+
+/**
+ *
+ * @author 503
+ */
 
 public class SocketClient {
     Socket socket = null;
 
     DataInputStream in;
     DataOutputStream out;
+    ObjectInputStream inObj;
+    ObjectOutputStream outObj;
     String ip = "localhost";
     int port = 10745;
 
@@ -36,6 +47,9 @@ public class SocketClient {
             socket = new Socket(ip, port);
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
+            inObj = new ObjectInputStream(socket.getInputStream());
+            outObj = new ObjectOutputStream(socket.getOutputStream());
+            
             while (true) {
 //                Chờ server gửi về khi nào gửi thì chayyj tiếp
 //                System.out.println("SERVER: " + ">>>>" + this.receive());
@@ -60,3 +74,4 @@ public class SocketClient {
 
     }
 }
+
